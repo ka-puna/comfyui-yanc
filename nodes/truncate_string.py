e61269cb4503a2fa9efadffdcf4fca9cf82533c0
Adam Wong <tue91717@temple.edu>
2023-11-15 17:28:18 -0500

    Add Truncate String node.

---
+++ nodes/truncate_string.py
@@ -0,0 +1,51 @@
+"""
+This node shortens a string by word, thken appends a trailing character to the
+        when it is shortened.
+        See: https://docs.python.org/3/library/textwrap.html#textwrap.shorten.
+Depends On:
+    textwrap
+Input:
+    string
+    length: The maximum character length of the string.
+    trail: The string suffixed to the output string when it is truncated.
+Ouput:
+    STRING: A copy of 'string' shortened by word to 'length', using 'trail'
+            to suffix a shortened string.
+"""
+from textwrap import shorten
+
+
+class TruncateString:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "string": ("STRING", { "forceInput": True,}),
+                "length": ("INT", { "default": 48,}),
+                "trail": ("STRING", { "default": "...",}),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("STRING",)
+    FUNCTION = "truncate_string"
+    OUTPUT_NODE = True
+    CATEGORY = "utils"
+
+    def truncate_string(self, string, length, trail):
+        return (shorten(string, width=length, placeholder=trail),)
+
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "YANC.TruncateString": TruncateString,
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "YANC.TruncateString": "Truncate String",
+}
